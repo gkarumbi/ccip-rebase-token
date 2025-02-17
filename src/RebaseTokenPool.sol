08fc35b24022c283111150cc582cab1d66bda134
gkarumbi <gkarumbi@gmail.com>
2025-02-17 12:18:49 +0300

    -Add RebaseTokenPool contract that makes use of Chainlink CCIP standard - TokenPool contract to create our own TokenPool

---
+++ src/RebaseTokenPool.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24 ;
+
+import{TokenPool} from "@ccip/contracts/src/v0.8/ccip/pools/TokenPool.sol";
+
+
+contract RebaseTokenPool is TokenPool{
+    constructor(IERC20 _token, address[] memory _allowlist, address _rnmProxy, address _router) TokenPool(_token, 18, _allowlist, _rnmProxy, _router){
+
+    }
+}
