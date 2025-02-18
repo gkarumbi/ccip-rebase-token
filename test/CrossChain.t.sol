ef25a3d4230cf31f45c87e1adbca86df54a696f6
gkarumbi <gkarumbi@gmail.com>
2025-02-18 15:20:22 +0300

    Added our tokens to the tokens registry, added deplloyer contract as the admin to the token and the token pool, connect token pool admins to their tokens

--- test/CrossChain.t.sol
+++ test/CrossChain.t.sol
@@ -5,22 +5,79 @@ import {Test, console} from "forge-std/Test.sol";
 import {RebaseToken} from "../src/RebaseToken.sol";
 import {Vault} from "../src/Vault.sol";
 import{RebaseTokenPool} from "../src/RebaseTokenPool.sol";
+import {IERC20} from "@ccip/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
 
-import {CCIPLocalSimulatorFork} from "@chainlink-local/src/ccip/CCIPLocalSimulatorFork.sol";
+import {CCIPLocalSimulatorFork,Register} from "@chainlink-local/src/ccip/CCIPLocalSimulatorFork.sol";
+
+import{RegistryModuleOwnerCustom} from "@ccip/contracts/src/v0.8/ccip/tokenAdminRegistry/RegistryModuleOwnerCustom.sol";
+import{TokenAdminRegistry} from "@ccip/contracts/src/v0.8/ccip/tokenAdminRegistry/TokenAdminRegistry.sol";
 
 import{IRebaseToken} from "../src/interfaces/IRebaseToken.sol";
 
 contract CrossChainTest is Test{
+    address constant owner = makeAddr("owner");
     uint256 sepoliaFork;
     uint256 arbSepoliaFork;
-    CCIPLocalSimulatorFork ccipLocalSimulatorFork;
-    function setUp() public{
 
-        sepoliaFork = vm.createSelectFor("sepolia-eth");
+    CCIPLocalSimulatorFork ccipLocalSimulatorFork;
+
+    RebaseToken sepoliaToken;
+    RebaseToken arbSepoliaToken;
+
+    Vault vault;
+
+    RebaseTokenPool sepoliaPool;
+    RebaseTokenPool arbSepoliaPool;
+
+    Register.NetworkDetails sepoliaNetworkDetails;
+    Register.NetworkDetails arbSepoliaNetworkDetails;
+
+    function setUp() public{
+        //make sure we are initially deploying on sepolia
+        sepoliaFork = vm.createSelectFork("sepolia-eth");
         arbSepoliaFork = vm.createFork("arb-sepolia");
 
         ccipLocalSimulatorFork = new CCIPLocalSimulatorFork();
         vm.makePersistent(address(ccipLocalSimulatorFork));
+
+        //1. Deploy and configure on sepolia
+        sepoliaNetworkDetails = ccipLocalSimulatorFork.getNetworkDetails(block.chainid);
+        vm.startPrank(owner);
+        sepoliaToken = new RebaseToken;
+        vault = new Vault(IRebaseToken(address(sepoliaToken)));
+        sepoliaPool = new RebaseTokenPool(IERC20(address(sepoliaToken)), new address[](0), sepoliaNetworkDetails._rmnProxy, sepoliaNetworkDetails._router);
+
+        sepoliaToken.grantMintAndBurnRole(address(vault));
+        sepoliaToken.grantMintAndBurnRole(address(sepoliaPool));
+        RegistryModuleOwnerCustom(sepoliaNetworkDetails.registyModuleOwnerCustomAddress).registerAdminViaOwner(address(sepoliaToken));
+        TokenAdminRegistry(sepoliaNetworkDetails.tokenAdminRegistryAddress).acceptAdminRole(address(sepoliaToken));
+        TokenAdminRegistry(sepoliaNetworkDetails.tokenAdminRegistryAddress).setPool(address(sepoliaToken), address(sepoliaPool));
+
+        vm.stopPrank();
+
+        //2. Deploy and configure on arbitrum
+        //change what we are interacting with to arbitrum sepolia
+        vm.selectFork(arbSepoliaFork);
+        arbSepoliaToken = new RebaseToken();
+        arbSepoliaNetworkDetails = ccipLocalSimulatorFork.getNetworkDetails(block.chainid);
+        arbSepoliaPool = new RebaseTokenPool(IERC20(address(arbSepoliaToken)), new address[](0), arbSepoliaNetworkDetails._rmnProxy, arbSepoliaNetworkDetails._router);
+
+        arbSepoliaToken.grantMintAndBurnRole(address(arbSepoliaPool));
+
+        //register token admin owner
+        RegistryModuleOwnerCustom(arbSepoliaNetworkDetails.registyModuleOwnerCustomAddress).registerAdminViaOwner(address(arbSepoliaToken));
+
+        //assign admin role to token pool
+        TokenAdminRegistry(arbSepoliaNetworkDetails.tokenAdminRegistryAddress).acceptAdminRole(address(arbSepoliaToken));
+
+        // connect the two
+        TokenAdminRegistry(arbSepoliaNetworkDetails.tokenAdminRegistryAddress).setPool(address(arbSepoliaToken), address(arbSepoliaPool));
+
+        vm.startPrank(owner);
+
+        vm.stopPrank();
+
+
         
 
     }
